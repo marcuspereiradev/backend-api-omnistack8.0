03fad04d8112b2096a98954de32734ae5c7a8023
marcuspereiradev <vinnycoast@gmail.com>
2019-08-06 13:50:54 -0300

    Testing post method

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,9 @@ routes.get('/', (req, res) => {
   return res.json({ message: `Hello ${req.query.name}` });
 });
 
+routes.post('/devs', (req, res) => {
+  console.log(req.body);
+  return res.json(req.body);
+});
+
 module.exports = routes;
