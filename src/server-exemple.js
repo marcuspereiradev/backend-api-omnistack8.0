ba532ff1438df3628be64fd92b670961162dc93d
marcuspereiradev <vinnycoast@gmail.com>
2019-08-07 15:09:55 -0300

    server-exemple

---
+++ src/server-exemple.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+
+// const routes = require('./routes');
+
+// const server = express();
+// // Replace <username>:<password> for your username e and password to connect your database
+// // Exemple.: mongodb+srv://myusername:mypassword@cluster0-fa981.mongodb.net...
+// mongoose.connect('mongodb+srv://<username>:<password>@cluster0-fa981.mongodb.net/test?retryWrites=true&w=majority', {
+//   useNewUrlParser: true
+// });
+
+// server.use(cors());
+// server.use(express.json());
+// server.use(routes);
+
+// server.listen(3333);
